59adffdbd9ac9063740180ee0cb7e027277ff2f3
Toby Retallick <toby.retallick@gmail.com>
2014-03-03 17:12:58 +0000

    Extracted common methods to module

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,23 +1,26 @@
-require 'bike'
+# link to the Bike class
+require './lib/bike'
 
-# we're describing the funcitonality of a specific class
-
+# we're describing the functionality of a specific class, Bike
 describe Bike do
 
   let(:bike) { Bike.new }
-  
-  it "should not be broken after we create it" do 
+
+  # this is a specific feature (behaviour) that we expect to be present
+  it "should not be broken after we create it" do    
+    # expect an instance of this class to have a method "broken?" that should return false
     expect(bike).not_to be_broken
   end
 
-  it "should be able to break" do
+  it "should be able to break" do    
     bike.break
     expect(bike).to be_broken
   end
 
-  it "should be able to get fixed" do
+  it "should be able to get fixed" do    
     bike.break
     bike.fix
     expect(bike).not_to be_broken
   end
+
 end
