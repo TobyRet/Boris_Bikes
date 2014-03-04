162a09213829bb057f4de902130394644f2c22fc
Toby Retallick <toby.retallick@gmail.com>
2014-03-04 15:03:29 +0000

    Created Van class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,10 @@ require './lib/docking_station'
 
 describe DockingStation do
     
-  let(:station) { DockingStation.new(:capacity => 123) }
+  let(:station) { DockingStation.new(:capacity => 100) }
 
   it "should allow setting default capacity on initialising" do
-    expect(station.capacity).to eq(123)
+    expect(station.capacity).to eq(100)
   end
 
 end
---
+++ spec/van_spec.rb
@@ -0,0 +1,11 @@
+require 'van'
+
+describe Van do
+
+  let(:van) { Van.new(:capacity => 20) }
+
+  it "should allow setting default capacity on initialising" do
+    expect(van.capacity).to eq(20)
+  end
+
+end
