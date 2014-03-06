bce1038beed94f50365a79378a4e6c55394bcbd8
Toby Retallick <toby.retallick@gmail.com>
2014-03-06 13:50:57 +0000

    created basic garage class

---
+++ spec/garage_spec.rb
@@ -0,0 +1,11 @@
+require './lib/garage'
+
+describe Garage do 
+
+  let(:garage) { Garage.new(:capacity => 200) }
+
+  it "should allow setting default capacity on initialising" do
+    expect(garage.capacity).to eq(200)
+  end
+  
+end
