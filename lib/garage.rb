b014a67f4b5b3a170f97aaa245a88a30e4f0cfe0
Toby Retallick <toby.retallick@gmail.com>
2014-03-07 09:28:37 +0000

    Garage fixes broken bikes delivered by van

--- lib/garage.rb
+++ lib/garage.rb
@@ -8,4 +8,12 @@ class Garage
     self.capacity = options.fetch(:capacity, capacity)
   end
 
+  def fix_broken_bikes_delivered_by(van)
+    van.broken_bikes.each do |bike|
+      van.release(bike)
+      self.dock(bike)
+      bike.fix
+    end
+  end
+
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,11 +1,22 @@
 require './lib/garage'
+require './lib/van'
+require './lib/bike'
 
 describe Garage do 
 
+  let(:van) { Van.new }
   let(:garage) { Garage.new(:capacity => 200) }
 
   it "should allow setting default capacity on initialising" do
     expect(garage.capacity).to eq(200)
   end
-  
+ 
+  it "should fix broken bikes delivered by the van" do
+    broken_bike = Bike.new
+    broken_bike.break 
+    van.dock(broken_bike)
+    garage.fix_broken_bikes_delivered_by(van)
+    expect(van).to be_empty
+    expect(garage.available_bikes.length).to eq(1)
+  end
 end
